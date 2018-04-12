f792508c16802303556b068a493141c7c8ba42aa
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2018-04-12 17:12:48 +0200

    Update Index.ts

--- src/Index.ts
+++ src/Index.ts
@@ -1,4 +1,4 @@
-import { Emitter, EmitOptions } from '@fluffy-spoon/javascript.csharp-to-typescript-generator';
+import { Emitter, EmitOptions } from '@fluffy-spoon/csharp-to-typescript-generator';
 
 var through = require('through2');
 var gutil = require('gulp-util');
